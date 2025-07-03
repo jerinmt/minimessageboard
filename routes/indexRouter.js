ed2ceba2c56ccd01622c7a6732b032c1e7b6a44e
jerinmt <jerinmt379@gmail.com>
2025-07-03 16:20:55 +0530

    Corrected messages array

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,14 +2,16 @@ const { Router } = require("express");
 const db = require("../db/queries");
 
 const indexRouter = Router();
-const currentMessages = db.getAllMessages();
-const messages = Object.entries(currentMessages);
 const links = [
   { href: "/", text: "Home" },
   { href: "new", text: "New" },
 ];
 
-indexRouter.get("/", (req, res) => res.render("index", { links: links, messages: messages }));
+indexRouter.get("/", (req, res) => {
+  const currentMessages = db.getAllMessages();
+  const messages = Object.values(currentMessages);
+  res.render("index", { links: links, messages: messages });
+});
 indexRouter.get("/new", (req, res) =>   res.render("form", { links: links }));
 indexRouter.post("/new", (req, res) => {
     const authorName = req.body.authorName;
